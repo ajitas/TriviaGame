0d541d4f9c069188c127779b6218d88cbb99ff58
Ajita Srivastava <ajitas4000@gmail.com>
2018-08-09 18:20:35 -0700

    added style change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,130 +1,189 @@
+//data structure to store the info about the questions
 var quiz = [
     {question:"what is 2+2?",
     options:[4,6,8,9],
-    answer:4},
+    answer:"4"},
     {question:"what is 20-10?",
     options:[4,6,8,10],
-    answer:10},
+    answer:"10"},
     {question:"what is 2*7?",
     options:[4,14,8,9],
-    answer:14},
+    answer:"14"},
     {question:"what is 6*6?",
     options:[4,6,36,9],
-    answer:36},
+    answer:"36"},
     {question:"what is 2+2?",
     options:[4,6,8,9],
-    answer:4},
+    answer:"4"},
     {question:"what is 20-10?",
     options:[4,6,8,10],
-    answer:10},
+    answer:"10"},
     {question:"what is 2*7?",
     options:[4,14,8,9],
-    answer:14},
+    answer:"14"},
     {question:"what is 6*6?",
     options:[4,6,36,9],
-    answer:36}
+    answer:"36"},
+    {question:"what is 2+2?",
+    options:[4,6,8,9],
+    answer:"4"},
+    {question:"what is 20-10?",
+    options:[4,6,8,10],
+    answer:"10"},
+    {question:"what is 2*7?",
+    options:[4,14,8,9],
+    answer:"14"},
+    {question:"what is 6*6?",
+    options:[4,6,36,9],
+    answer:"36"}
 ];
 
-var currentTime =10;
+//global counters
+var currentTime =60;
 var updateTime;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 
+//when the page loads
 $(document).ready(function(){
+    //reset the game
     resetGame();
 });
 
+//when user clicks start game button
 $("#start-game").on("click",function(){
+
+    //show/hide appropriate divs
     $("#start-game").hide();
     $("#time-remaining").show();
     $("#question-area").show();
     $("#finish-game").show();
+    //show timer
     $("#time-remaining").append("<p>" + timeConvert(currentTime)+"</p>");
 
+    //start the timer
     clearInterval(updateTime);
     updateTime = setInterval(decrementTime,1000);
 
+    //for each question
     for(var i =0; i<quiz.length;i++){
 
+        //create a div for showing the question
         var questionDiv = $("<div>");
         questionDiv.html(i+1 + ". " +quiz[i]["question"]);
+        //append the newly created question div to question-area
         $("#question-area").append(questionDiv);
+
+        //for each of the four options
         for(var j =0; j<4 ;j++){
 
+            //create a radio button with id=option1, option2
             var optionRadio = $("<input type='radio'>");
             optionRadio.attr("id","option"+parseInt(j+1));
+            //give the radio button name of option1 for first question, option2 for second..
             optionRadio.attr("name","question"+parseInt(i+1));
+            //give it the value from the option array
             optionRadio.val(quiz[i]["options"][j]);
 
+            //craete a label and give it text from the option array
             var label = $("<label for=option"+parseInt(j+1)+">"); 
             label.text(quiz[i]["options"][j]);
 
+            //create a new div and append radio button to it and give it the label created earlier
             var optionDiv = $("<div>");
             optionDiv.append(optionRadio);
             optionDiv.append(label);
             
+            //append the new div to the question-area after the question
             $("#question-area").append(optionDiv);
 
         }
     }
 });
 
+//when questions are submitted
 $("#finish-game").on("click", function(){
+    //evaluate how the player did
     evaluateAnswers();
 });
 
+//decrements the time remaining every second
 function decrementTime(){
+    //decrement the current time by 1 second
     currentTime--;
+    //remove the previous time from the time-remaining div
     $("#time-remaining > p").remove();
+    //add the new time to the time-remaining div
     $("#time-remaining").append("<p>" + timeConvert(currentTime)+"</p>");
-    if(currentTime == 0){
+
+    //if time becomes 0, your time is up and the evaluation of the game is shown
+    if(currentTime === 0){
+        //evaluate how the player did
         evaluateAnswers();
     }
 }
 
+//evaluates the answers after player submits it or when time is up
 function evaluateAnswers(){
 
+    //stop the timer
     clearInterval(updateTime);
 
+    //hide/show appropriate divs
     $("#evaluation-area").show();
     $("#time-remaining").hide();
     $("#question-area").hide();
     $("#finish-game").hide();
 
+    //for each of the question
     for(var i =0; i<quiz.length;i++){
 
-        if(quiz[i]["answer"] ===  parseInt($("input[name=question"+parseInt(i+1)+"]:checked").val())){
+        //if the answer matches with the user input
+        if(quiz[i]["answer"] ===  $("input[name=question"+parseInt(i+1)+"]:checked").val()){
+            //increment the correct counter
             correct++;
         }
+        //if user did not answer
         else if (!$("input[name=question"+parseInt(i+1)+"]:checked").val())
         {
+            //increment the unanswered counter
             unanswered++;
         }
+        //if  the answer does not match user input increment the incorrect counter
         else
         incorrect++;
     }
+    //create a new div to show the evaluation
     var correctDiv = $("<div>");
     correctDiv.html("Correct Answers: " + correct);
 
+    //create a new div to show the evaluation
     var incorrectDiv = $("<div>");
     incorrectDiv.html("Incorrect Answers: " + incorrect);
 
+    //create a new div to show the evaluation
     var unansweredDiv = $("<div>");
     unansweredDiv.html("Unanswered: " + unanswered);
 
+    //append the three newly created divs to result div
     $("#evaluation-area").append(correctDiv);
     $("#evaluation-area").append(incorrectDiv);
     $("#evaluation-area").append(unansweredDiv);
 
 }
+
+//resets the game 
 function resetGame(){
+    //start the game and show /hide appropriate areas
     $("#start-game").show();
     $("#time-remaining").hide();
     $("#question-area").hide();
     $("#finish-game").hide();
     $("#evaluation-area").hide();
 }
+
+//converts the time into required format to show in the time-remaining div
 function timeConvert(currentTime){
     var minutes = Math.floor(currentTime / 60);
     var seconds = currentTime - (minutes * 60);
